5b08b578f1ea0d70d5c840305c46adefea41d585
CreeperTNTea <103012790+CreeperTNTea@users.noreply.github.com>
2022-04-21 20:47:37 +0800

    Add files via upload

--- DoMath/headfile/ratio.h
+++ DoMath/headfile/ratio.h
@@ -4,24 +4,16 @@
 #ifndef SAMKHYA
 #include"samkhya.h"
 #endif
+#ifndef STRUCTURE
+#include "structure.h"
+#endif
 
 class ratio{
-    //ratio:p/q
-    friend ratio ratio_create(long a,short b);
+    //ratio=p/q
     private:
     long p=0;
-    short q=1;
+    unsigned short q=1;
     public:
-    void set(long m,short n=1){
-        p=m;
-        q=n;
-    };
-    long getp(){
-        return p;
-    };
-    long getq(){
-        return q;
-    };
     void simplify(){
         long x=gcd(p,q);
         p/=x;
@@ -31,9 +23,23 @@ class ratio{
             p=~p+1;
         };
     };
-    ratio(long a=0,short b=1){
+    inline ratio(long a=0,unsigned short b=1){
+        if(b<0){
+            q=~b+1;
+            p=~a+1;
+        }
         p=a;
         q=b;
+    };
+    void operator()(long a=0,unsigned short b=1){
+        p=a;
+        q=b;
+    };
+    long getp(){
+        return p;
+    };
+    unsigned short getq(){
+        return q;
     };
     bool operator==(const ratio& m){
         if(this->p*m.q==this->q*m.p) return true;
@@ -43,51 +49,108 @@ class ratio{
         p=m.p;
         q=m.q;
     };
+    void operator=(const short& m){
+        p=m;
+        q=1;
+    };
     void operator+=(const ratio& m){
         p*=m.q;
         p+=m.p*q;
         q*=m.q;
+    };
+    void operator+=(const short& m){
+        p+=m*q;
+        simplify();
     };
     void operator-=(const ratio& m){
         p*=m.q;
         p-=m.p*q;
         q*=m.q;
     };
+    void operator-=(const short& m){
+        p-=m*q;
+        simplify();
+    };
     void operator*=(const ratio& m){
         p*=m.p;
         q*=m.q;
+    };
+    void operator*=(const short& m){
+        p*=m;
+        simplify();
     };
     void operator/=(const ratio& m){
         p*=m.q;
         q*=m.p;
     };
+    void operator/=(const short& m){
+        q*=m;
+        simplify();
+    };
+    ratio operator-(){
+        return ratio(~p+1,q);
+    };
     ratio operator+(const ratio& m){
-        ratio ans;
-        ans.p=p*m.q+q*m.p;
-        ans.q=q*m.q;
+        ratio ans(p*m.q+q*m.p,q*m.q);
+        ans.simplify();
         return ans;
+    };
+    ratio operator+(const short& m){
+        return ratio(p+m*q,q);
     };
     ratio operator-(const ratio& m){
-        ratio ans;
-        ans.p=this->p*m.q-this->q*m.p;
-        ans.q=this->q*m.q;
+        ratio ans(p*m.q-q*m.p,q*m.q);
+        ans.simplify();
         return ans;
     };
+    ratio operator-(const short& m){
+        return ratio(p-m*q,q);
+    };
     ratio operator*(const ratio& m){
-        ratio ans;
-        ans.p=this->p*m.p;
-        ans.q=this->q*m.q;
+        ratio ans(p*m.p,q*m.q);
+        ans.simplify();
         return ans;
     };
     ratio operator/(const ratio& m){
-        ratio ans;
-        ans.p=this->p*m.q;
-        ans.q=this->q*m.p;
+        ratio ans(p*m.q,q*m.p);
+        ans.simplify();
         return ans;
+    };
+    bool operator>(const ratio& m){
+        return p/q>m.p/m.q;
+    };
+    bool operator<(const ratio& m){
+        return p/q<m.p/m.q;
+    };
+    void operator++(){
+        p+=q;
+    };
+    void operator--(){
+        p-=q;
     };
     friend std::ostream& operator<<(std::ostream& output,const ratio& m){
         output<<'('<<m.p<<'/'<<m.q<<')';
         return output;
+    };
+    friend std::istream& operator>>(std::istream& input,ratio& m){
+        input>>m.p>>m.q;
+        if(input){
+            m.simplify();
+        }
+        else{
+            m.p=0;
+            m.q=1;
+        }
+        return input;
+    };
+    explicit operator int (){
+        return p/q;
+    };
+    explicit operator float (){
+        return p/(float)q;
+    };
+    explicit operator double (){
+        return p/(double)q;
     };
     void add(short m){
         p+=q*m;
@@ -102,66 +165,25 @@ class ratio{
     void div(short m){
         q*=m;
     };
-    int to_integer(){
-        return p/q;
-    };
-    float to_float(){
-        return p/q;
-    };
     ratio abs(){
         simplify();
-        if(p<0){
-            ratio ans;
-            ans.p=~p+1;
-            ans.q=q;
-            return ans;
-        }
-        return *this;
+        return ratio(mthpabs(p),q);
     };
-    ratio pow(const short& n){
+    ratio pow(const unsigned char& n=2){
         ratio ans;
         ans.simplify();
         ans.p=pow_integer(p,n);
         ans.q=pow_integer(q,n);
         return ans;
     };
-    friend ratio add(const ratio& m,const short& n);
-    friend ratio sub(const ratio& m,const short& n);
-    friend ratio mul(const ratio& m,const short& n);
-    friend ratio div(const ratio& m,const short& n);
+    void read(std::string notice="ratio=p/q,(p,q)="){
+        std::cout<<'\n'<<notice<<":\n";
+        std::cin>>p>>q;
+    };
+    void from_float(float& m){
+        p=60000*m;
+        q=60000;
+    };
 };
-ratio ratio_create(long a,short b=1){
-    ratio ans;
-    ans.p=a;
-    ans.q=b;
-    return ans;
-};
-ratio add(const ratio& m,const short& n){
-    ratio ans=m;
-    ans.add(n);
-    return ans;
-};
-ratio sub(const ratio& m,const short& n){
-    ratio ans=m;
-    ans.sub(n);
-    return ans;
-};
-ratio mul(const ratio& m,const short& n){
-    ratio ans=m;
-    ans.mul(n);
-    return ans;
-};
-ratio div(const ratio& m,const short& n){
-    ratio ans=m;
-    ans.div(n);
-    return ans;
-};
-ratio to_ratio(short n){
-    return ratio_create(n,1);
-};
-ratio to_ratiof(float n){
-    ratio ans;
-    ans.set((long)10000*n,10000);
-    ans.simplify();
-    return ans;
-};
+
+//
